6d3cd354b4457be73a37dd30e14ba56e0212e362
vladimirKutyavin <kva.devops@gmail.com>
2021-11-25 13:52:14 +0400

    0. Spring Liquibase [#302330 #232209] - correct

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,4 +1,4 @@
-create table if not exists authorities (
+create table authorities (
                              id serial primary key,
                              authority varchar (50) not null unique
 );
@@ -19,12 +19,10 @@ create table posts (
                        created timestamp without time zone not null default now(),
                        user_id int references users(id)
 );
-
 create table comments (
                           id serial primary key,
                           description varchar (2000),
                           created timestamp without time zone not null default now(),
                           post_id int not null references posts(id),
                           user_id int not null references users(id)
-);
-
+)
