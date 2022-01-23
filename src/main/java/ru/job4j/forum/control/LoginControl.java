ddb3c935a596ad091ff8476e0a952996443a29b0
vladimirKutyavin <kva.devops@gmail.com>
2022-01-23 18:09:17 +0400

    replace LoginControl

--- src/main/java/ru/job4j/forum/control/LoginControl.java
+++ src/main/java/ru/job4j/forum/control/LoginControl.java
@@ -1,4 +1,4 @@
-package ru.job4j.forum;
+package ru.job4j.forum.control;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
